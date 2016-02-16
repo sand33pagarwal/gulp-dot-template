641499a209a50ba16a4947132a139716acefa028
Constantin Titarenko <constantin.titarenko@gmail.com>
2016-02-17 00:15:08 +0200

    keep defaults

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ var dot = require('dot');
 module.exports = render;
 
 function render (data, options) {
-	options = options || { strip: false };
+	options = options || {
+		evaluate: /\{\{([\s\S]+?)\}\}/g,
+		interpolate: /\{\{=([\s\S]+?)\}\}/g,
+		encode: /\{\{!([\s\S]+?)\}\}/g,
+		use: /\{\{#([\s\S]+?)\}\}/g,
+		define: /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+		conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+		iterate: /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+		varname: 'it',
+		strip: false,
+		append: true,
+		selfcontained: false
+	};
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			cb(null, file);
